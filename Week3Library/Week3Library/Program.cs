d30a6897123bcbaecd67ef2d5394665d32bacd9b
B00982308 <Mcgaughey-t@ulster.ac.uk>
2025-10-06 15:03:00 +0100

    update

---
+++ Week3Library/Week3Library/Program.cs
@@ -0,0 +1,31 @@
+﻿class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+
+    void displayinfo()
+    {
+
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Author Name: {Author}");
+        Console.WriteLine($"Books ISBN: {ISBN}");
+        Console.WriteLine($"");
+    }
+    static void Main(string[] args)
+    {
+        Book book = new Book();
+        book.Title = "The Idiot";
+        book.Author = "Fyodor dostoevsky";
+        book.ISBN = "17384444";
+
+        Book bok = new Book();
+        bok.Title = "the genius";
+        bok.Author = "Taylor Mcgaughey";
+        bok.ISBN = "12112500";
+
+        bok.displayinfo();
+        book.displayinfo();
+
+    }
+}
